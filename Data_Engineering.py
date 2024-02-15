a7cc051de176b7206205cbba3a4a271ec1cc85e7
Devops-13999 <dev377627@gmail.com>
2024-02-16 00:55:48 +0530

    python project containerization interactive mode

---
+++ Data_Engineering.py
@@ -0,0 +1,5 @@
+print('Program to sum two numbers')
+num1 = int(input('Enter the first number'))
+num2 = int(input('Enter the first number'))
+result = num1 + num2
+print(f'sum of two numbers are {result}')
